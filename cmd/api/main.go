a623e1e682903a8d9e7ac37166120548d54bb52d
Arda Özcan <150556426+DinVisel@users.noreply.github.com>
2026-04-08 23:33:03 +0300

    RabbitMQ implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,68 +4,93 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	_ "github.com/lib/pq"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/redis/go-redis/v9"
 )
 
 var (
-	db  *sql.DB
-	rdb *redis.Client
-	ctx = context.Background()
+	db       *sql.DB
+	rdb      *redis.Client
+	amqpChan *amqp.Channel
+	ctx      = context.Background()
 )
 
 func main() {
+	// DB redis connection
+	initDBAndRedis()
 
-	//DB Connection
-	var err error
-	connStr := "postgres://user:password@localhost:5434/ticket_db?sslmode=disable"
-	db, err = sql.Open("postgres", connStr)
+	// RabbitMQ connection
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	defer conn.Close()
 
-	// Redis connection
-	rdb = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-
-	// CRITICAL: sync stock quantity from DB to redis
-	var stock int
-	err = db.QueryRow("SELECT stock FROM tickets WHERE id = 1").Scan(&stock)
+	amqpChan, err = conn.Channel()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	rdb.Set(ctx, "ticket_stock:1", stock, 0)
+	defer amqpChan.Close()
+
+	// Define queue
+	_, err = amqpChan.QueueDeclare("ticket_queue", true, false, false, false, nil)
+
+	// Start worker
+	go startDBWorker()
 
 	r := chi.NewRouter()
-	r.Post("/buy", buyWithRedisShield)
+	r.Post("/buy", buyWithQueue)
 
-	fmt.Println("Redis shield active! Server runs at port: 8080")
+	fmt.Println("Event-Driven System is ready! Port: 8080")
+	fmt.Println("RabbitMQ Dashboard: http://localhost:15672")
 	http.ListenAndServe(":8080", r)
 }
 
-func buyWithRedisShield(w http.ResponseWriter, r *http.Request) {
-	//Step A: decrease stock from redis
-	newStock, err := rdb.Decr(ctx, "ticket_stock:1").Result()
-	if err != nil {
-		http.Error(w, "Redis Error", http.StatusInternalServerError)
-		return
-	}
-
-	//Step B: if new stock is lower than 0 tickets sold out
+func buyWithQueue(w http.ResponseWriter, r *http.Request) {
+	// STEP 1: Redis Check
+	newStock, _ := rdb.Decr(ctx, "ticket_stock:1").Result()
 	if newStock < 0 {
 		http.Error(w, "Tickets sold out!", http.StatusConflict)
 		return
 	}
 
-	//Step C: Redis approved, update DB quietly
-	_, err = db.Exec("UPDATE tickets SET stock = stock - 1 WHERE id = 1")
+	// STEP 2: Add message to queue (async)
+	body := "1" // Selled ticket id
+	err := amqpChan.PublishWithContext(ctx, "", "ticket_queue", false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+
 	if err != nil {
-		fmt.Println("DB could not updated: ", err)
+		http.Error(w, "Queue error", 500)
+		return
 	}
 
-	w.Write([]byte("Your ticket purchased succesfully (redis approved)"))
+	w.Write([]byte("Your process added to queue. Ticket is getting ready!"))
+}
+
+func startDBWorker() {
+	msgs, _ := amqpChan.Consume("ticket_queue", "", true, false, false, false, nil)
+
+	for d := range msgs {
+		log.Printf("Message taken from queue: %s. DB updating...", d.Body)
+		_, err := db.Exec("UPDATE tickets SET stock = stock - 1 WHERE id = 1")
+		if err != nil {
+			log.Println("DB Hatası:", err)
+		}
+	}
+}
+
+func initDBAndRedis() {
+	db, _ = sql.Open("postgres", "postgres://user:password@localhost:5434/ticket_db?sslmode=disable")
+	rdb = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+
+	// Initialize DB with redis at start
+	rdb.Set(ctx, "ticket_stock:1", 100, 0)
+	db.Exec("UPDATE tickets SET stock = 100 WHERE id = 1")
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi/v5 v5.2.5 // indirect
 	github.com/lib/pq v1.12.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
 	github.com/redis/go-redis/v9 v9.18.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 )
