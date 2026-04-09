f825ea020e8d189d1d93fc1dd33afd5cec7095a5
Arda Özcan <150556426+DinVisel@users.noreply.github.com>
2026-04-09 15:07:10 +0300

    created ticket service file and purchasing functionality. also made some minor fixes

--- internal/repository/postgres_redis.go
+++ internal/repository/postgres_redis.go
@@ -8,32 +8,32 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type TicketRepo struct {
+type ticketRepo struct {
 	db  *sql.DB
 	rdb *redis.Client
 }
 
-func NewTicketRepository(db *sql.DB, rdb *redis.Client) *TicketRepo {
-	return &TicketRepo{db: db, rdb: rdb}
+func NewTicketRepository(db *sql.DB, rdb *redis.Client) *ticketRepo {
+	return &ticketRepo{db: db, rdb: rdb}
 }
 
-func (r *TicketRepo) DecrementStockCache(ctx context.Context, id int) (int64, error) {
+func (r *ticketRepo) DecrementStockCache(ctx context.Context, id int) (int64, error) {
 	key := fmt.Sprintf("ticket_stock:%d", id)
 	return r.rdb.Decr(ctx, key).Result()
 }
 
-func (r *TicketRepo) UpdateStockDB(ctx context.Context, id int, quantity int) error {
+func (r *ticketRepo) UpdateStockDB(ctx context.Context, id int, quantity int) error {
 	_, err := r.db.ExecContext(ctx, "UPDATE tickets SET stock = stock - $1 WHERE id = $2 AND stock >= $1", quantity, id)
 	return err
 }
 
-func (r *TicketRepo) GetStock(ctx context.Context, id int) (int, error) {
+func (r *ticketRepo) GetStock(ctx context.Context, id int) (int, error) {
 	var stock int
 	err := r.db.QueryRowContext(ctx, "SELECT stock FROM tickets WHERE id = $1", id).Scan(&stock)
 	return stock, err
 }
 
-func (r *TicketRepo) SyncCacheWithDB(ctx context.Context, id int) error {
+func (r *ticketRepo) SyncCacheWithDB(ctx context.Context, id int) error {
 	stock, err := r.GetStock(ctx, id)
 	if err != nil {
 		return err
---
+++ internal/service/ticket_service.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/DinVisel/EventDrivenTicketSystem/internal/domain"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type ticketService struct {
+	repo     domain.TicketRepository
+	amqpChan *amqp.Channel
+}
+
+func NewTicketService(repo domain.TicketRepository, ch *amqp.Channel) domain.TicketService {
+	return &ticketService{
+		repo:     repo,
+		amqpChan: ch,
+	}
+}
+
+func (s *ticketService) Purchase(ctx context.Context, ticketID int) error {
+	// try decreasing quickly from Redis
+	newStock, err := s.repo.DecrementStockCache(ctx, ticketID)
+	if err != nil {
+		return fmt.Errorf("Cache error: %w", err)
+	}
+
+	// if stock is empty (minus) reject immediately
+	if newStock < 0 {
+		return errors.New("Unfortunately tickets sold out")
+	}
+
+	// message to queue (async)
+	// Do not make user wait until db update
+	msg := fmt.Sprintf("%d", ticketID)
+	err = s.amqpChan.PublishWithContext(ctx,
+		"",
+		"ticket_queue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msg),
+		})
+
+	if err != nil {
+		// CRITICAL: if we cannot add to queue, we should return the place in redis. (Rollback)
+		return fmt.Errorf("Queue error: %w", err)
+	}
+	return nil
+}
