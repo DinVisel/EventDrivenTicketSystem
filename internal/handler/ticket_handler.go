e02af18c252a4d67ade8eb0995e737efd0543a13
Arda Özcan <150556426+DinVisel@users.noreply.github.com>
2026-04-09 17:52:20 +0300

    Created ticket handler to seperate service and http requests

---
+++ internal/handler/ticket_handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/DinVisel/EventDrivenTicketSystem/internal/domain"
+	"github.com/go-chi/chi/v5"
+)
+
+type TicketHandler struct {
+	service domain.TicketService
+}
+
+func NewHandler(s domain.TicketService) *TicketHandler {
+	return &TicketHandler{service: s}
+}
+
+func (h *TicketHandler) BuyTicket(w http.ResponseWriter, r *http.Request) {
+	// Take ticketID from url
+	ticketIDStr := chi.URLParam(r, "ticketID")
+	ticketID, _ := strconv.Atoi(ticketIDStr)
+
+	// Call service layer
+	err := h.service.Purchase(r.Context(), ticketID)
+
+	// return response
+	if err != nil {
+		w.WriteHeader(http.StatusConflict)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Your request has been queued!"})
+}
